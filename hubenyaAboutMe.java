9ed7e35ad2361faa841dbf4bf04f4409e0e3372c
ahubenya <ahubenya@gmail.com>
2017-10-10 00:54:49 -0700

    finished homework

--- hubenyaAboutMe.java
+++ hubenyaAboutMe.java
@@ -2,13 +2,13 @@ public class angelaHubenya {
     public static void main(String[] args) {
         System.out.println("Hello, my name is angelaHubenya");
         System.out.println("I have been a software test engineer for 5+ years");
-        System.out.println("I am ");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
-        System.out.println("I am taking this");
+        System.out.println("I am taking this class so I can learn programming just a little bit");
+        System.out.println("I am married to an SDET in the making");
+        System.out.println("I have a 20 month old daughter");
+        System.out.println("My daughters name is Amelia ");
+        System.out.println("For fun, I like to spend time with my friends and family");
+        System.out.println("I like to bake, rollerblade, hike, and travel");
+        System.out.println("I am not bothered my this crazy Seattle traffic");
+        System.out.println("But I do get overwhelmed when I cannot find parking in Seattle!");
     }   
    }
